d42cdb6b0427b7d35608fed114ad101a503c8e71
peebilly <peekcnvs@gmail.com>
2026-04-22 10:17:30 +0700

    fix: Update YouTube embed URL
    The previous YouTube embed URL was incorrect. This commit updates it to the correct URL for the Dopamine Melody explainer video.

--- src/App.tsx
+++ src/App.tsx
@@ -386,7 +386,7 @@ const VideoSection = () => {
           {/* YouTube Video Player */}
           <iframe 
             className="w-full h-full"
-            src="https://www.youtube.com/embed/ebfQ_NMPkIE"
+            src="https://www.youtube.com/embed/jANCqyec64U?si=RE9qFyAo820gIncB"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
